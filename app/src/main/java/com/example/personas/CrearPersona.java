b8299833f64d0e46fefa2ddd72db38cb63e06920
“ivan” <ibanb040@gmail.com>
2020-11-14 17:04:16 -0500

    Listar personas funciona

--- app/src/main/java/com/example/personas/CrearPersona.java
+++ app/src/main/java/com/example/personas/CrearPersona.java
@@ -3,13 +3,43 @@ package com.example.personas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class CrearPersona extends AppCompatActivity {
+   private EditText cedula, nombre, apellido;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_persona);
+        cedula = findViewById(R.id.idCedula);
+        nombre = findViewById(R.id.idNombre);
+        apellido = findViewById(R.id.idApellido);
     }
 
+    public void guardar(View v){
+        String ced, nom, apell;
+        Persona p;
+        ced = cedula.getText().toString();
+        nom = nombre.getText().toString();
+        apell = apellido.getText().toString();
+        p = new Persona(ced,nom,apell);
+        p.guardar();
+        Toast.makeText(this, R.string.mensaje_guardado_exitosamente,
+                Toast.LENGTH_LONG).show();
+        limpiar();
+    }
+
+    public void limpiar(View v){
+        limpiar();
+    }
+
+    public void limpiar(){
+        cedula.setText("");
+        nombre.setText("");
+        apellido.setText("");
+        cedula.requestFocus();
+    }
 }
